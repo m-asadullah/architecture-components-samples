c24d37d351a7805f58a6b65ef683ebcd14cb1bfd
M. AsadUllah <68208088+m-asadullah@users.noreply.github.com>
2024-09-01 22:50:34 -0700

    GithubBrowserSample: abstract object is only for abstract member - Object is not abstract and does not implement abstract member public abstract fun onChanged(value: T): Unit defined in androidx.lifecycle.Observer

--- GithubBrowserSample/app/src/test-common/java/com/android/example/github/util/LiveDataTestUtil.kt
+++ GithubBrowserSample/app/src/test-common/java/com/android/example/github/util/LiveDataTestUtil.kt
@@ -30,14 +30,14 @@ import java.util.concurrent.TimeoutException
  * `InstantTaskExecutorRule` or a similar mechanism to execute tasks synchronously.
  */
 fun <T> LiveData<T>.getOrAwaitValue(
-        time: Long = 2,
-        timeUnit: TimeUnit = TimeUnit.SECONDS,
-        afterObserve: () -> Unit = {}
+    time: Long = 2,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+    afterObserve: () -> Unit = {}
 ): T {
     var data: T? = null
     val latch = CountDownLatch(1)
     val observer = object : Observer<T> {
-        override fun onChanged(o: T?) {
+        override fun onChanged(o: T) {
             data = o
             latch.countDown()
             this@getOrAwaitValue.removeObserver(this)
